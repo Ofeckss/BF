553ee61a5f3af7f0de5fa42d19d9e38175a682a4
Jose523rt <2402072@utrivieramaya.edu.mx>
2026-07-02 19:00:29 -0500

    Fix de variable no existente

--- front/src/components/TradeConfirmModal.vue
+++ front/src/components/TradeConfirmModal.vue
@@ -314,7 +314,6 @@ async function verificarStatus() {
 onMounted(async () => {
   console.log('[TradeConfirmModal] montado con chatId:', props.chatId, '| articuloId:', props.articuloId)
   await cargarTodo()
-  console.log('Es venta? ', auth.isVenta)
   // refresca lo que agrega el otro usuario. 7s en vez de 4s para no saturar
   // el backend con más polling encima del que ya hace el chat de mensajes.
   pollingInterval = setInterval(refrescarDetalles, 10000)
